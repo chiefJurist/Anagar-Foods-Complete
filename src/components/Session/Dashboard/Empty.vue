be18c886312e363dd4b1773acbef541b3dead152
chiefJurist <anthonynnanna004@gmail.com>
2024-01-20 06:10:17 +0100

    update

--- src/components/Session/Dashboard/Empty.vue
+++ src/components/Session/Dashboard/Empty.vue
@@ -1,3 +1,12 @@
+<script>
+    export default{
+        data () {
+            return {
+                dashboard: true
+            }
+        }
+    }
+</script>
 <template>
-    <div class="session-empty">You Have No Orders!</div>
+    <div class="session-empty" v-if="!dashboard">You Have No Orders!</div>
 </template>
