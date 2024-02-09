0883ec5cb08722b240ec05783dc218680faf5104
chiefJurist <anthonynnanna004@gmail.com>
2024-02-09 19:22:17 +0100

    update history

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -6,10 +6,40 @@
     export default{
         components: { Empty, Record, Title},
 
+        props: [ 'id' ],
+
         data () {History
             return{
-                history: true
+                history: false,
+                result: ""
             }
+        },
+
+        mounted () {
+            //Fetch data from the database
+            fetch("http://localhost/Anagar-Foods-Complete/data/dashboard.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify({
+                    id: this.id
+                })
+            })
+            //fetching the response
+            .then((response) => response.json())
+            //fetching the actual data and populating the result property
+            .then((data) => {
+                this.result = data;
+                if (this.result.status  != "empty" &&  this.result.status  != "error") {
+                    this.history = true;
+                    console.log(this.result)
+                } else {
+                    this.history = false;
+                }
+            })
+            //catching errors
+            .catch((error) => console.error(error))
         }
     }
 </script>
@@ -18,6 +48,6 @@
     <div class=" bg-bg-7">
         <Title v-if="history"></Title>
         <Empty v-if="!history"></Empty>
-        <Record v-if="history"></Record>
+        <Record v-if="history" :result="result"></Record>
     </div>
 </template>
