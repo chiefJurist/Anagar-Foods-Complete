fec00ca83b0905abf50df8f0d50eeda116943325
chiefJurist <anthonynnanna004@gmail.com>
2024-02-19 04:10:10 +0100

    update

--- src/components/About/LocalMenu.vue
+++ src/components/About/LocalMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="about-con flex-row-reverse">
         <!--Image-->
-        <div class=" flex-1 bg-bg-4 bg-no-repeat">
+        <div class=" flex-1 bg-bg-4 bg-no-repeat transition duration-1000 ease-out" v-motion-slide-visible-left>
             <img src="/img/img-5.jpg" alt="" class="  rounded-full border-8 border-yellow-400 w-full object-cover">  
         </div>
 
@@ -10,23 +10,23 @@
         <div class=" flex-1">
             <div class=" card-title ">Local Menu</div>
             <ul class="text-yellow-600">
-                <li class="list-style">Jollof Rice</li>
-                <li class="list-style">Fried Rice</li>
-                <li class="list-style">Coconut Rice</li>
-                <li class="list-style">Palm Oil Rice</li>
-                <li class="list-style">White Rice</li>
-                <li class="list-style">Spaghetti</li>
-                <li class="list-style">Macaroni</li>
-                <li class="list-style">Boiled Yam</li>
-                <li class="list-style">Porridge Yam</li>
-                <li class="list-style">Beans</li>
-                <li class="list-style">Boiled / Fried Irish potato</li>
-                <li class="list-style">Boiled / Fried Sweet Potato</li>
-                <li class="list-style">Semolina</li>
-                <li class="list-style">Semovita</li>
-                <li class="list-style">Casava mash (fufu)</li>
-                <li class="list-style">Casava Flakes Pudding ( garri)</li>
-                <li class="list-style">Wheat</li>
+                <li class="list-style" v-motion-slide-visible-right>Jollof Rice</li>
+                <li class="list-style" v-motion-slide-visible-right>Fried Rice</li>
+                <li class="list-style" v-motion-slide-visible-right>Coconut Rice</li>
+                <li class="list-style" v-motion-slide-visible-right>Palm Oil Rice</li>
+                <li class="list-style" v-motion-slide-visible-right>White Rice</li>
+                <li class="list-style" v-motion-slide-visible-right>Spaghetti</li>
+                <li class="list-style" v-motion-slide-visible-right>Macaroni</li>
+                <li class="list-style" v-motion-slide-visible-right>Boiled Yam</li>
+                <li class="list-style" v-motion-slide-visible-right>Porridge Yam</li>
+                <li class="list-style" v-motion-slide-visible-right>Beans</li>
+                <li class="list-style" v-motion-slide-visible-right>Boiled / Fried Irish potato</li>
+                <li class="list-style" v-motion-slide-visible-right>Boiled / Fried Sweet Potato</li>
+                <li class="list-style" v-motion-slide-visible-right>Semolina</li>
+                <li class="list-style" v-motion-slide-visible-right>Semovita</li>
+                <li class="list-style" v-motion-slide-visible-right>Casava mash (fufu)</li>
+                <li class="list-style" v-motion-slide-visible-right>Casava Flakes Pudding ( garri)</li>
+                <li class="list-style" v-motion-slide-visible-right>Wheat</li>
             </ul>            
         </div>
     </div>
