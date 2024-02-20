208a41e3f4cdbacd28b25f368325fa95ef4cfc2c
chiefJurist <anthonynnanna004@gmail.com>
2024-02-20 19:43:43 +0100

    update

--- src/components/Session/Dashboard/Welcome2.vue
+++ src/components/Session/Dashboard/Welcome2.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class=" text-2xl font-serif sm:text-3xl md:text-5xl text-orange-600 text-center font-bold pt-32 pb-7 transition duration-1000 ease-out" v-motion-slide-visible-bottom>Welcome To Anagar Foods</div>
-        <div class=" bg-clip-text text-transparent bg-gradient-to-r from-yellow-200 to-orange-600 text-2xl sm:text-3xl md:text-5xl font-extrabold py-12 sm:pb-16 text-right transition duration-1000 ease-out" v-motion-slide-visible-left>Your Orders Below</div>
+        <div class=" text-2xl font-serif sm:text-3xl md:text-5xl text-orange-600 text-center font-bold pt-32 pb-7">Welcome To Anagar Foods</div>
+        <div class=" bg-clip-text text-transparent bg-gradient-to-r from-yellow-200 to-orange-600 text-2xl sm:text-3xl md:text-5xl font-extrabold py-12 sm:pb-16 text-right">Your Orders Below</div>
     </div>
 </template>
